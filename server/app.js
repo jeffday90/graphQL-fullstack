b44e97249bb235faa8a7698473e293dfe2042569
Jeff Day <jeffrey.day00@gmail.com>
2020-06-08 13:10:11 -0700

    Relational querying.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema.js');
 
 const app = express();
 const PORT = 3000;
 
 app.use('/graphql', graphqlHTTP({
-
+  schema,
+  graphiql: true,
 }));
 
 app.listen(PORT, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,28 +1,76 @@
 const graphql = require('graphql');
+const _ = require('lodash');
+const { 
+    GraphQLObjectType,
+    GraphQLSchema, 
+    GraphQLString, 
+    GraphQLID,
+    GraphQLInt,
+ } = graphql;
 
-// pulls schema class from the package
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+// dummy data
+const books = [
+    { name: 'book one', genre: 'sci-fi', id: '1', authorID: '1' },
+    { name: 'book 2', genre: 'fantasy', id: '2', authorID: '2' },
+    { name: 'book 3', genre: 'sci-fi', id: '3', authorID: '1' },
+    { name: 'book FOUR', genre: 'fantasy', id: '4', authorID: '3' }
+]
 
-// defined first object type for the graphql server
+const authors = [
+    { name: 'Jeff', age: 24, id: '1'},
+    { name: 'Alfie', age: 99, id: '2'},
+    { name: 'Lorraine', age: 4, id: '3'},
+]
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+          type: AuthorType,
+          resolve(parent, args) {
+            console.log(parent);
+            return _.find(authors, { id: parent.authorID })
+          }
+        }
     })
 })
 
-// when somone makes a query, expect some arguments to be passed in
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    })
+})
+
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
       // when someone queries a book, use the below code to go and get it
       book: {
         type: BookType,
-        args: { id: { type: GraphQLString }},
+        args: { id: { type: GraphQLID }},
         resolve(parent, args){ 
             // this is the function where we write code to get data from DB/ other source
+
+            // uses lodash to look through books for id that matches args.id
+            return _.find(books, { id: args.id });
+            
+        }
+      },
+      author: {
+        type: AuthorType,
+        args: { id: { type: GraphQLID }},
+        resolve(parent, args){ 
+            // this is the function where we write code to get data from DB/ other source
+
+            // uses lodash to look through author for id that matches args.id
+            return _.find(authors, { id: args.id });
             
         }
       }
