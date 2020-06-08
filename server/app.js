32fe9d1559539770d3e19f611ddaab50ae3e2709
Jeff Day <jeffrey.day00@gmail.com>
2020-06-08 14:48:47 -0700

    Connected mongo isntance to app.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema.js');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3000;
+
+mongoose.connect('mongodb+srv://graphql-tutorial-rmu1a.mongodb.net/graphql-tutorial', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
+
+mongoose.connection.once('open', () => {
+    console.log('connected to DB');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
